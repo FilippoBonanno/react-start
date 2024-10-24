dc1d448d3cc194f80262e8a5f260a5946f81fddc
FilippoBonanno <fbonanno15@gmail.com>
2024-10-24 06:05:46 +0200

    Rendering condizionale &&

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 import img_1 from './assets/img-1.png'
@@ -15,16 +13,16 @@ function App() {
   return (
     <>
       <div className="grid grid-cols-4 gap-5">
-        <Card title="Tokyo" imgUrl="https://media.gettyimages.com/id/904453184/it/foto/immagine-composita-del-monte-fuji-e-dello-skyline-di-tokyo.jpg?s=612x612&w=gi&k=20&c=DjeMEHuejLb7rCaCHFmPodDIGl91e5QuWWY2gTSiAp8=">
+        <Card isVisited={true} title="Tokyo" imgUrl="https://media.gettyimages.com/id/904453184/it/foto/immagine-composita-del-monte-fuji-e-dello-skyline-di-tokyo.jpg?s=612x612&w=gi&k=20&c=DjeMEHuejLb7rCaCHFmPodDIGl91e5QuWWY2gTSiAp8=">
           Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.
         </Card>
-        <Card title="New York" imgUrl="https://img.freepik.com/foto-gratuito/splendida-vista-degli-stati-impero-e-dei-grattacieli-di-new-york-city_181624-6295.jpg">
+        <Card isVisited={false} title="New York" imgUrl="https://img.freepik.com/foto-gratuito/splendida-vista-degli-stati-impero-e-dei-grattacieli-di-new-york-city_181624-6295.jpg">
           Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.
         </Card>
-        <Card title="Paris" imgUrl="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8fA%3D%3D">
+        <Card isVisited={true} title="Paris" imgUrl="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8fA%3D%3D">
           Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.
         </Card>
-        <Card title="London" imgUrl="https://img.freepik.com/foto-gratuito/big-ben-e-westminster-bridge-al-tramonto-londra-regno-unito_268835-1395.jpg">
+        <Card isVisited={false} title="London" imgUrl="https://img.freepik.com/foto-gratuito/big-ben-e-westminster-bridge-al-tramonto-londra-regno-unito_268835-1395.jpg">
           Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.
         </Card>
       </div>
@@ -34,15 +32,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
 
-      <img className={`${active == 1 ? "block" : "hidden"}`} src={img_1} alt="" />
-      <img className={`${active == 2 ? "block" : "hidden"}`} src={img_2} alt="" />
-      <img className={`${active == 3 ? "block" : "hidden"}`} src={img_3} alt="" />
-      <button onClick={active == 1 ? () => setCount(2) : () => setCount(1)} className='btn btn-primary'>Next Image</button>
+
 
     </>
   )
--- react-project/src/components/Card.jsx
+++ react-project/src/components/Card.jsx
@@ -1,4 +1,4 @@
-function Card({ title, imgUrl, children }) {
+function Card({ title, imgUrl, isVisited, children }) {
 
 
 
@@ -9,7 +9,12 @@ function Card({ title, imgUrl, children }) {
             <img className="w-full" src={imgUrl} alt=""></img>
             <div className="flex flex-col p-4">
                 <h2 className="text-2xl font-bold text-white" >{title}</h2>
-                <p className="text-gray-500">{children}</p>
+                <p className="text-gray-500 mb-2">{children}</p>
+                {/* SE è VERO FAI VEDERE QUESTO ALTRIMENTI QUELLO */}
+                <span>{isVisited ? "Visited" : "Not Visited"}</span>
+                {/* E ALLORA FAI VEDERE QUESTO  */}
+                <span>{isVisited && "My Favorite"}</span>
+
             </div>
 
         </div>
